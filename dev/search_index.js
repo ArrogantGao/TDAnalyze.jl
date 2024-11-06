1e0106817ae78ae9fe5c833bac0778183f920eec
Documenter.jl <documenter@juliadocs.github.io>
2024-11-06 06:07:41 +0000

    build based on d3d879e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TDAnalyze","category":"page"},{"location":"#TDAnalyze","page":"Home","title":"TDAnalyze","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TDAnalyze.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TDAnalyze]","category":"page"}]
+}
